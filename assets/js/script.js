9a243dd2caa9435293be8dd3c0efd59fb995f746
Jonathan Agatston <jagatston123@gmail.com>
2021-03-27 23:33:13 -0400

    made changes to the layout of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+var formModal = $("#form-modal");
+var openModalButton = $("#open-modal-button");
+var currentDateTimeSpan = $("#current-date-time");
+
+
+function updateCurrentTime(){
+    currentDateTimeSpan.text()
+}
+
+openModalButton.on("click", function () {
+    formModal.modal("show");
+});
+
+console.log(moment().format("MMM Do YYYY, hh:mm:ss"));
